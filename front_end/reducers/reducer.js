32968510e37dd6554549e312a449e2311505ade5
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-01 15:21:59 -0700

    Moved store back to /src

--- front_end/reducers/reducer.js
+++
@@ -1,5 +0,0 @@
-const initialState = {};
-
-export const reducer = (state = initialState, action) => {
-  return state;
-}
--- front_end/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from 'redux'
-
-import { reducer } from './reducers';
-
-export default createStore(reducer, window.REDUX_DEVTOOLS_EXTENSION && window.REDUX_DEVTOOLS_EXTENSION());
