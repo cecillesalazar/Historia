2293148af6f3d2aa9437472077cdbca44c5fecc9
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-01 15:23:01 -0700

    Imported Provider from react-redux

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import Gameplay from './components/gameplay';
+import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Gameplay />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Gameplay />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
