3076a454de0c4d3d2cc63b044acc827c46efff45
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-01 17:32:39 -0700

    Edited config to hook up mLab database

--- back_end/config.js
+++ back_end/config.js
@@ -4,13 +4,8 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+        process.env.DATABASE_URL || 'mongodb://cecillesalazar:csalazar1@ds163781.mlab.com:63781/historia-users',
   TEST_DATABASE_URL:
         process.env.TEST_DATABASE_URL ||
         'mongodb://localhost/thinkful-backend-test'
-  // DATABASE_URL:
-  //     process.env.DATABASE_URL || 'postgres://localhost/thinkful-backend',
-  // TEST_DATABASE_URL:
-  //     process.env.TEST_DATABASE_URL ||
-  //     'postgres://localhost/thinkful-backend-test'
 };
