23b5e762a2d0e48a9c848a4829f989028897d364
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-04 16:13:42 -0700

    Dispatched changeNode action on next button click - hardcoded node to test

--- front_end/src/components/gameplay.js
+++ front_end/src/components/gameplay.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './components-css/login.css';
 import { connect } from 'react-redux';
 import helper from './helper.js';
+import { changeNode } from '../actions/actions.js';
 
 class Gameplay extends React.Component {
-
   render() {
     return (
       <div>
         <h1>{this.props.storyNode.story}</h1>
-        <button type="button" className="next-button">Next</button>
+        <button type="button" onClick={() => this.props.dispatch(changeNode('SERG_TELLS_STORY'))}>Next</button>
       </div>
     )
   }
