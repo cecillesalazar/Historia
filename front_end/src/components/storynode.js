0c0eb8b45154bb899e7f47a4bfc420cb18d67b5f
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-04 16:37:18 -0700

    Received jsx from gameplay.js to storynode.js

--- front_end/src/components/storynode.js
+++ front_end/src/components/storynode.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import helper from './helper.js';
+import { changeNode } from '../actions/actions.js';
 
-export default function StoryNode(props) {
-  return {
-    
+class StoryNode extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{this.props.storyNode.story}</h1>
+        <button type="button" onClick={() => this.props.dispatch(changeNode(this.props.storyNode.options[0]))}>Next</button>
+      </div>
+    )
   }
+
 }
+
+function mapStateToProps(state) {
+  return { storyNode: helper.getStoryNode(state.currentStoryNodeKey) }
+}
+
+export default connect(mapStateToProps)(StoryNode);
