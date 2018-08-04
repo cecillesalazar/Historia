ad552402b6870b150c8cf032e643888089c8cb4c
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-04 10:09:52 -0700

    Imported and implemented Provider in index.js

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import Gameplay from './components/gameplay';
 import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
+  <Provider store={store}>
     <Gameplay
       storyNode={{
         key: 'SERG_ARRIVES',
@@ -14,6 +16,7 @@ ReactDOM.render(
         ],
         story: 'Plot for SERG_ARRIVES',
       }}
-    />,
+    />
+  </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
