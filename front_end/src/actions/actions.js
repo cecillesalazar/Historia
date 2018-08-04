53740d09c29072b31ef23f4bd30796aea727ce9e
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-04 16:13:14 -0700

    Exported changeNode into gameplay.js

--- front_end/src/actions/actions.js
+++ front_end/src/actions/actions.js
@@ -1,5 +1,5 @@
 const CHANGE_NODE = 'CHANGE_NODE';
-const changeNode = function(currentNode) {
+export const changeNode = (currentNode) => ({
   type: CHANGE_NODE,
-  currentNode
-}
+  currentNode,
+})
