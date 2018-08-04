b343c03eb6a9c9c9419b7122cab5955ab468120f
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-04 16:36:20 -0700

    Moved JSX for story nodes to storynode.js

--- front_end/src/components/gameplay.js
+++ front_end/src/components/gameplay.js
@@ -1,22 +1,15 @@
 import React from 'react';
-import { connect } from 'react-redux';
 import helper from './helper.js';
 import { changeNode } from '../actions/actions.js';
+import StoryNode from './storynode.js';
 
-class Gameplay extends React.Component {
+export default class Gameplay extends React.Component {
   render() {
     return (
       <div>
-        <h1>{this.props.storyNode.story}</h1>
-        <button type="button" onClick={() => this.props.dispatch(changeNode(this.props.storyNode.options[0]))}>Next</button>
+        <StoryNode />
       </div>
     )
   }
 
 }
-
-function mapStateToProps(state) {
-  return { storyNode: helper.getStoryNode(state.currentStoryNodeKey) }
-}
-
-export default connect(mapStateToProps)(Gameplay);
