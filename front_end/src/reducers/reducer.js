29b033d50a074d11b3714bf9a55feca11d61c498
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-05 14:21:03 -0700

    Created another key in the state for saving the story node and added save node action in reducer

--- front_end/src/reducers/reducer.js
+++ front_end/src/reducers/reducer.js
@@ -1,13 +1,18 @@
 import helper from '../components/helper.js';
 
 const initialState = {
-  currentStoryNodeKey: helper.SERG_ARRIVES
+  currentStoryNodeKey: helper.SERG_ARRIVES,
+  saveFileStoryNodeKey: helper.SERG_ARRIVES,
 };
 
 export const reducer = (state = initialState, action) => {
   if(action.type === 'CHANGE_NODE') {
     return Object.assign({}, state, {
       currentStoryNodeKey: action.currentNode
+    })
+  } else if(action.type === 'SAVE_NODE') {
+    return Object.assign({}, state, {
+      saveFileStoryNodeKey: action.nodeToSave
     })
   } else {
     return state;
