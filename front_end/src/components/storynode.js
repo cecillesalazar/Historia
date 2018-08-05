9a131a65b3860ccc7e2503b4a52813f55138950c
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-05 08:11:28 -0700

    Added conditional for rending options with 1 option or 2 options

--- front_end/src/components/storynode.js
+++ front_end/src/components/storynode.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import helper from './helper.js';
+import { StoryNodeOptions } from './storynodeoptions.js';
 import { changeNode } from '../actions/actions.js';
 
 class StoryNode extends React.Component {
   render() {
-    return (
-      <div>
-        <h1>{this.props.storyNode.story}</h1>
-        <button type="button" onClick={() => this.props.dispatch(changeNode(this.props.storyNode.options[0]))}>Next</button>
-      </div>
-    )
+    if(this.props.storyNode.options.length === 1) {
+      return (
+        <div>
+          <h1>{this.props.storyNode.story}</h1>
+          <button type="button" onClick={() => this.props.dispatch(changeNode(this.props.storyNode.options[0]))}>Next</button>
+        </div>
+      )
+    } else {
+      return <div>Hello world</div>
+    }
   }
 
 }
