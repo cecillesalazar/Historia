d4aa2d74ff0045d680ccd2f586a698e7d79fc4d5
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-05 09:41:17 -0700

    Utilized changeStoryNode prop to storynodeoptions.js

--- front_end/src/components/storynodeoptions.js
+++ front_end/src/components/storynodeoptions.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-export default function StoryNodeOptions(props) {
-    return (
-      <div>
-        Hello World
-      </div>
-    )
+export default class StoryNodeOptions extends React.Component {
+  render() {
+    if(this.props.options.length === 1) {
+      return (
+        <div>
+          <button type="button" onClick={() => this.props.changeStoryNode(this.props.options[0])}>Next</button>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <button type="button" onClick={() => this.props.changeStoryNode(this.props.options[0])}>'Wish For 200 Pounds'</button>
+          <button type="button" onClick={() => this.props.changeStoryNode(this.props.options[1])}>'Throw in Fire'</button>
+        </div>
+      )
+    }
+  }
 }
