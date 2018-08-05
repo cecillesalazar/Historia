4e982dfd63860d963ec8afeb3d9eea8befc8a1ee
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-05 13:07:41 -0700

    Got rid of if conditionals, used 1 map to cycle through the options and display in buttons

--- front_end/src/components/storynodeoptions.js
+++ front_end/src/components/storynodeoptions.js
@@ -2,21 +2,13 @@ import React from 'react';
 
 export default class StoryNodeOptions extends React.Component {
   render() {
-    if(this.props.options.length === 1) {
-      return (
-        <div>
-          <button type="button" onClick={() => this.props.changeStoryNode(this.props.options[0])}>Next</button>
-        </div>
-      )
-    } else if (this.props.options.length === 0) {
-      return '';
-    } else {
-      return (
-        <div>
-          <button type="button" onClick={() => this.props.changeStoryNode(this.props.options[0])}>{this.props.options[0]}</button>
-          <button type="button" onClick={() => this.props.changeStoryNode(this.props.options[1])}>{this.props.options[1]}</button>
-        </div>
-      )
-    }
+    const options = this.props.options.map(option => (
+      <button key={option} type="button" onClick={() => this.props.changeStoryNode(option)}>{option}</button>
+    ));
+    return (
+      <div>
+        {options}
+      </div>
+    )
   }
 }
