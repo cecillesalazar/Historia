11e3b92b629dfab7b57680e2133b10d497ac826b
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-05 08:43:46 -0700

    Removed conditional from storynode.js to be moved to storynodeoptions.js

--- front_end/src/components/storynode.js
+++ front_end/src/components/storynode.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import helper from './helper.js';
-import { StoryNodeOptions } from './storynodeoptions.js';
+import StoryNodeOptions from './storynodeoptions.js';
 import { changeNode } from '../actions/actions.js';
 
 class StoryNode extends React.Component {
   render() {
-    if(this.props.storyNode.options.length === 1) {
       return (
         <div>
           <h1>{this.props.storyNode.story}</h1>
-          <button type="button" onClick={() => this.props.dispatch(changeNode(this.props.storyNode.options[0]))}>Next</button>
+          <StoryNodeOptions />
         </div>
       )
-    } else {
-      return <div>Hello world</div>
-    }
   }
 
 }
