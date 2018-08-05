5cd1b7b714a17eae74164546f8071d927e386804
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-05 14:35:33 -0700

    Reverted our reducer back to before save feature changes

--- front_end/src/reducers/reducer.js
+++ front_end/src/reducers/reducer.js
@@ -1,18 +1,13 @@
 import helper from '../components/helper.js';
 
 const initialState = {
-  currentStoryNodeKey: helper.SERG_ARRIVES,
-  saveFileStoryNodeKey: helper.SERG_ARRIVES,
+  currentStoryNodeKey: helper.SERG_ARRIVES
 };
 
 export const reducer = (state = initialState, action) => {
   if(action.type === 'CHANGE_NODE') {
     return Object.assign({}, state, {
       currentStoryNodeKey: action.currentNode
-    })
-  } else if(action.type === 'SAVE_NODE') {
-    return Object.assign({}, state, {
-      saveFileStoryNodeKey: action.nodeToSave
     })
   } else {
     return state;
