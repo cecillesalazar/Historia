aecdf00eaefc730331128d898ccb7cf72bfadce9
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-05 09:25:31 -0700

    Cleaned up unneeded imports

--- front_end/src/components/gameplay.js
+++ front_end/src/components/gameplay.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import helper from './helper.js';
-import { changeNode } from '../actions/actions.js';
 import StoryNode from './storynode.js';
 
 export default class Gameplay extends React.Component {
