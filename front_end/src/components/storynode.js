6217718000c19c42d8f6431cb5fae1a4297fdd4f
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-05 14:20:17 -0700

    Imported save node action and placed save component in storynode.js

--- front_end/src/components/storynode.js
+++ front_end/src/components/storynode.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import helper from './helper.js';
-import { changeNode } from '../actions/actions.js';
+import { changeNode, saveNode } from '../actions/actions.js';
 import StoryNodeOptions from './storynodeoptions.js';
+import Save from './savefeature.js';
 
 class StoryNode extends React.Component {
   changeStoryNode = (storyNodeKey) => this.props.dispatch(changeNode(storyNodeKey));
-  
+  saveStoryNode = (storyNodeKey) => this.props.dispatch(saveNode(storyNodeKey));
+
   render() {
       return (
         <div>
@@ -14,6 +16,10 @@ class StoryNode extends React.Component {
           <StoryNodeOptions
             changeStoryNode={this.changeStoryNode}
             options={this.props.storyNode.options}
+          />
+          <Save
+            saveStoryNode={this.saveStoryNode}
+            key={this.props.storyNode.key}
           />
         </div>
       )
