2e3dda02e11011416e2b4a3ad15e85f48e6c40c3
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-06 13:52:24 -0700

    Edited backendApi.js to implement async call

--- front_end/src/backendApi.js
+++ front_end/src/backendApi.js
@@ -1,10 +1,15 @@
 // A temp variable to mikic using our database;
 // Ultimately we will need to make ajax requests to our server from this file.
-const gameSaves = [];
-global.gameSaves = gameSaves;
+import { saveFileSuccess } from './actions/actions.js';
 
-export const saveGame = async (nodeToSave) => {
-  gameSaves.push(nodeToSave);
+export const saveGame = saveFile => dispatch => {
+  return fetch('http://localhost:8080/5b6867ee107abe0f460f2eff', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({saveFile})
+  })
+  .then(res => res.json())
+  .then(storyNode => dispatch(saveFileSuccess(storyNode)));
 };
-
-export const getSaveStates = async () => gameSaves;
