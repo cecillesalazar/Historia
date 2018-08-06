1fc48ee7a9a32d5f8f50ef918f8f39d3d409d77c
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-06 08:24:06 -0700

    Removed required in saveFile

--- back_end/user-schema.js
+++ back_end/user-schema.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  saveFile: { type: String, required: true }
+  saveFile: { type: String }
 })
 
 userSchema.set('toObject', {
