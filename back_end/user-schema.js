798e57396aa1e97340a9a1e68dc3d0abdb7f39b0
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-06 08:08:22 -0700

    Edited user-schema.js to include built-in virtual id

--- back_end/user-schema.js
+++ back_end/user-schema.js
@@ -5,4 +5,12 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true }
 })
 
+userSchema.set('toObject', {
+  virtuals: true,     // include built-in virtual `id`
+  versionKey: false,  // remove `__v` version key
+  transform: (doc, ret) => {
+    delete ret._id; // delete `_id`
+  }
+});
+
 module.exports = mongoose.model('User', userSchema);
