7d60df0708f9082719cc6555a6b007afea452797
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-07 09:57:44 -0700

    Removed dispatch from saveGame function in backendApi.js

--- front_end/src/backendApi.js
+++ front_end/src/backendApi.js
@@ -2,7 +2,7 @@
 // Ultimately we will need to make ajax requests to our server from this file.
 import { saveFileSuccess } from './actions/actions.js';
 
-export const saveGame = saveFile => dispatch => {
+export const saveGame = saveFile => {
   return fetch('/5b6867ee107abe0f460f2eff', {
     method: 'PUT',
     headers: {
@@ -11,7 +11,7 @@ export const saveGame = saveFile => dispatch => {
     body: JSON.stringify({saveFile})
   })
   .then(res => res.json())
-  .then(storyNode => dispatch(saveFileSuccess(storyNode)));
+  .then(storyNode => saveFileSuccess(storyNode));
 };
 
 export const loadGame = () => {
