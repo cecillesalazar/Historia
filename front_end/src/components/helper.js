0b40dd0431584bcf16ecd47b9c42d8f2f9685588
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-07 16:36:15 -0700

    Refactored to compartmentalize the fuction a bit

--- front_end/src/components/helper.js
+++ front_end/src/components/helper.js
@@ -1,4 +1,14 @@
-const getStoryNode = (state) => { return state.currentStory.storyGraph[state.currentStoryNodeKey] };
+import { converge } from 'ramda'
+
+const getCurrentStory = (state) => state.currentStory;
+const getCurrentStoryGraph = (state) => getCurrentStory(state).storyGraph;
+const getCurrentStoryNodeKey = (state) => state.currentStoryNodeKey;
+
+
+const getStoryNode = converge(
+  (storyGraph, key) => storyGraph[key],
+  [getCurrentStoryGraph, getCurrentStoryNodeKey],
+)
 
 export default {
   getStoryNode
