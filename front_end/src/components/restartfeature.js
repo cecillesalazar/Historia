547a3318d00a725c5e3d23db9567099680d19404
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-07 12:51:34 -0700

    Edited restartfeature.js to not be hardcoded to SERG_ARRIVES

--- front_end/src/components/restartfeature.js
+++ front_end/src/components/restartfeature.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import helper from '../components/helper.js';
 
 export default class Restart extends React.Component {
   render() {
     return (
       <div>
-        <button type="button" onClick={() => this.props.changeStoryNode('SERG_ARRIVES')}>Restart</button>
+        <button type="button" onClick={() => this.props.changeStoryNode(helper.STORY_GRAPH['SERG_ARRIVES'].key)}>Restart</button>
       </div>
     )
   }
