dfd16dbf4c0ee4b89fb598177aa9a1ede69715ec
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-07 10:41:24 -0700

    Imported saveFileSuccess action and integrated it into savefeature.js

--- front_end/src/components/savefeature.js
+++ front_end/src/components/savefeature.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { saveGame } from '../backendApi.js'
+import { saveGame } from '../backendApi.js';
+import { saveFileSuccess } from '../actions/actions.js';
 
 export default class Save extends React.Component {
   render() {
     return (
       <div>
-        <button type="button" onClick={() => this.props.dispatch(saveGame(this.props.currentStoryNodeKey))}>Save</button>
+        <button type="button" onClick={() => saveGame(this.props.currentStoryNodeKey).then(this.props.dispatch(saveFileSuccess(this.props.currentStoryNodeKey)))}>Save</button>
       </div>
     )
   }
