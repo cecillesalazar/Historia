3ce79b1dc56ac40c98b923cd1c48406eee2c5100
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-07 14:37:35 -0700

    Added currentStoryGraph key in initial state and changeStory action in reducer

--- front_end/src/reducers/reducer.js
+++ front_end/src/reducers/reducer.js
@@ -2,6 +2,7 @@ import helper from '../components/helper.js';
 import { saveFileSuccess } from '../actions/actions.js';
 
 const initialState = {
+  currentStoryGraph: null,
   currentStoryNodeKey: helper.STORY_GRAPH['SERG_ARRIVES'].key
 };
 
@@ -13,6 +14,11 @@ export const reducer = (state = initialState, action) => {
   } else if(action.type === 'SAVE_FILE_SUCCESS') {
     return Object.assign({}, state, {
       currentStoryNodeKey: action.storyNode
+    })
+  } else if(action.type === 'CHANGE_STORY') {
+    return Object.assign({}, state, {
+      currentStoryGraph: action.story,
+      currentStoryNodeKey: action.story.startNode
     })
   } else {
     return state;
