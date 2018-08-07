ed8e16ca84a9dc306b96d9eeb84af0fb75ec4143
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-06 20:05:19 -0700

    Added GET fetch call in backendApi.js

--- front_end/src/backendApi.js
+++ front_end/src/backendApi.js
@@ -13,3 +13,10 @@ export const saveGame = saveFile => dispatch => {
   .then(res => res.json())
   .then(storyNode => dispatch(saveFileSuccess(storyNode)));
 };
+
+export const loadGame = dispatch => {
+  return fetch('http://localhost:8080/5b6867ee107abe0f460f2eff', {
+    method: 'GET'
+  })
+  .then(res => res.json(res.saveFile))
+}
