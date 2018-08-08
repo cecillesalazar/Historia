dee9f192d5eda0c93185ec452e273e264614bb2f
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-08 13:01:57 -0700

    Removed localStrategy from user.js - will be placed in auth folder

--- back_end/routes/user.js
+++ back_end/routes/user.js
@@ -7,45 +7,6 @@ const User = require('../user-schema.js');
 const router = express.Router();
 router.use(express.json());
 
-const localStrategy = new LocalStrategy((username, password, done) => {
-  let user;
-  User
-    .findOne({ username })
-    .then(results => {
-      user = results;
-
-      if (!user) {
-        return Promise.reject({
-          reason: 'LoginError',
-          message: 'Incorrect username',
-          location: 'username'
-        });
-      }
-
-      return user.validatePassword(password);
-    })
-    .then(isValid => {
-      if (!isValid) {
-        return Promise.reject({
-          reason: 'LoginError',
-          message: 'Incorrect password',
-          location: 'password'
-        });
-      }
-      return done(null, user);
-    })
-    .catch(err => {
-      if (err.reason === 'LoginError') {
-        return done(null, false);
-      }
-
-      return done(err);
-
-    });
-});
-
-passport.use(localStrategy);
-
 router.get('/:id', (req, res, next) => {
   const { id } = req.params;
 
