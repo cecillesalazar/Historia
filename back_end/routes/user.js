a82f00b4b7524cd4a3787f28ec47c57f8fb4e658
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-08 11:22:18 -0700

    Added validations and tweaked promise syntax to allow for them

--- back_end/routes/user.js
+++ back_end/routes/user.js
@@ -63,18 +63,112 @@ router.post('/', (req, res, next) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  const newUser = { username, password }
-  User
-    .create(newUser)
-    .then(result => {
-      res
-        .location(`${req.originalUrl}`)
-        .status(201)
-        .json(result)
-    })
-    .catch(err => {
-      next(err)
-    })
+  const requiredFields = ['username', 'password'];
+  const missingField = requiredFields.find(field => !(field in req.body));
+
+  if (missingField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Missing field',
+      location: missingField
+    });
+  }
+
+  const stringFields = ['username', 'password'];
+  const nonStringField = stringFields.find(
+    field => field in req.body && typeof req.body[field] !== 'string'
+  );
+
+  if (nonStringField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Incorrect field type: expected string',
+      location: nonStringField
+    });
+  }
+
+  const explicityTrimmedFields = ['username', 'password'];
+  const nonTrimmedField = explicityTrimmedFields.find(
+    field => req.body[field].trim() !== req.body[field]
+  );
+
+  if (nonTrimmedField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: 'Cannot start or end with whitespace',
+      location: nonTrimmedField
+    });
+  }
+
+  const sizedFields = {
+    username: {
+      min: 1
+    },
+    password: {
+      min: 8,
+      // bcrypt truncates after 72 characters, so let's not give the illusion
+      // of security by storing extra (unused) info
+      max: 72
+    }
+  };
+  const tooSmallField = Object.keys(sizedFields).find(
+    field =>
+      'min' in sizedFields[field] &&
+            req.body[field].trim().length < sizedFields[field].min
+  );
+  const tooLargeField = Object.keys(sizedFields).find(
+    field =>
+      'max' in sizedFields[field] &&
+            req.body[field].trim().length > sizedFields[field].max
+  );
+
+  if (tooSmallField || tooLargeField) {
+    return res.status(422).json({
+      code: 422,
+      reason: 'ValidationError',
+      message: tooSmallField
+        ? `Must be at least ${sizedFields[tooSmallField]
+          .min} characters long`
+        : `Must be at most ${sizedFields[tooLargeField]
+          .max} characters long`,
+      location: tooSmallField || tooLargeField
+    });
+  }
+
+  return
+    User
+      .find({username})
+      .count()
+      .then(count => {
+        if (count > 0) {
+          // There is an existing user with the same username
+          return Promise.reject({
+            code: 422,
+            reason: 'ValidationError',
+            message: 'Username already taken',
+            location: 'username'
+          });
+        }
+        return User.hashPassword(password);
+      })
+      .then(digest => {
+        return User.create({
+          username,
+          password: digest
+        });
+      })
+      .then(user => {
+        return res.status(201).location(`${req.originalUrl}`).json(user.serialize());
+      })
+      .catch(err => {
+        if (err.reason === 'ValidationError') {
+          return res.status(err.code).json(err);
+        }
+        res.status(500).json({code: 500, message: 'Internal server error'});
+      });
 })
 
 router.put('/:id', (req, res, next) => {
