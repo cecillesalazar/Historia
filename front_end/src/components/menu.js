1d9800143b7779ab86690fe18f95b6be7a8c0bd7
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-08 07:28:53 -0700

    Created menu component with Play and How to Play buttons

---
+++ front_end/src/components/menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Menu extends React.Component {
+  render() {
+    return (
+      <div>
+        <button type="button" onClick={}>Play</button>
+        <button type="button">How to Play</button>
+      </div>
+    )
+  }
+
+}
