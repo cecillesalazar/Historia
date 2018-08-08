b98c45d51aae23ad62906fd6b27089d9d0a68504
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-08 16:29:19 -0700

    Accidentally deleted a file - restored it

--- front_end/src/components/dashboard.js
+++ front_end/src/components/dashboard.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import requiresLogin from './requires-login';
-import {fetchProtectedData} from '../actions/protected-data';
 
 export class Dashboard extends React.Component {
-    componentDidMount() {
-        this.props.dispatch(fetchProtectedData());
-    }
 
     render() {
         return (
@@ -22,9 +18,7 @@ export class Dashboard extends React.Component {
 const mapStateToProps = state => {
     const {currentUser} = state.auth;
     return {
-        username: state.auth.currentUser.username,
-        name: `${currentUser.firstName} ${currentUser.lastName}`,
-        protectedData: state.protectedData.data
+        username: state.auth.currentUser.username
     };
 };
 
