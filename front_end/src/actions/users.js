e7d9b7a423a57001bc6f556f768cfe3c00d7df32
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-08 13:55:46 -0700

    Added users.js into actions folder

---
+++ front_end/src/actions/users.js
@@ -0,0 +1,25 @@
+import {SubmissionError} from 'redux-form';
+import {normalizeResponseErrors} from './utils';
+
+export const registerUser = user => dispatch => {
+    return fetch(`/users`, {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+        .then(res => normalizeResponseErrors(res))
+        .then(res => res.json())
+        .catch(err => {
+            const {reason, message, location} = err;
+            if (reason === 'ValidationError') {
+                // Convert ValidationErrors into SubmissionErrors for Redux Form
+                return Promise.reject(
+                    new SubmissionError({
+                        [location]: message
+                    })
+                );
+            }
+        });
+};
