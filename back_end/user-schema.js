babe85f3013b3986b03651d41bbe72b901906ca4
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-07 17:35:57 -0700

    Added bcrypt methods into the schema

--- back_end/user-schema.js
+++ back_end/user-schema.js
@@ -6,4 +6,12 @@ const userSchema = new mongoose.Schema({
   saveFile: { type: String }
 })
 
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+};
+
+userSchema.statics.hashPassword = function(password) {
+  return bcrypt.hash(password, 10);
+}
+
 module.exports = mongoose.model('User', userSchema);
