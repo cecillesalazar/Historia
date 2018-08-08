4db82074400cb4ea3309c9f50595f51a7919885d
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-08 09:25:07 -0700

    Made username unique inuser-schema.js

--- back_end/user-schema.js
+++ back_end/user-schema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   saveFile: { type: String }
 })
