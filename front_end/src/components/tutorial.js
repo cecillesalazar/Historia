33660cf58e5472921a22150d92b17186ffbc31d4
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-08 08:38:03 -0700

    Created tutorial component - empty for now

---
+++ front_end/src/components/tutorial.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Tutorial extends React.Component {
+  render() {
+    return (
+      <div>
+        Place Instructions Here - Preferably as a Modal
+      </div>
+    )
+  }
+
+}
