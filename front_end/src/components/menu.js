eb126a9b67b8aaf657bd22e686417aee093d4bb5
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-08 08:12:52 -0700

    Built out menu component to conditionally display the gameplay component

--- front_end/src/components/menu.js
+++ front_end/src/components/menu.js
@@ -1,13 +1,40 @@
 import React from 'react';
+import Gameplay from './gameplay.js';
+import { connect } from 'react-redux';
+import { displayGameplay } from '../actions/actions.js';
 
-export default class Menu extends React.Component {
-  render() {
-    return (
-      <div>
-        <button type="button" onClick={}>Play</button>
-        <button type="button">How to Play</button>
-      </div>
-    )
+class Menu extends React.Component {
+  constructor(props) {
+    super(props)
+    this.displayGameplay = this.displayGameplay.bind(this);
   }
 
+  displayGameplay() {
+    !this.props.playButton ? this.props.dispatch(displayGameplay(true)) : ''
+  }
+
+  render() {
+    if(this.props.playButton) {
+      return (
+        <div>
+          <Gameplay />
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <button type="button" onClick={() => this.displayGameplay()}>Play</button>
+          <button type="button">How to Play</button>
+        </div>
+      )
+    }
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    playButton: state.playButton
+   }
+}
+
+export default connect(mapStateToProps)(Menu);
