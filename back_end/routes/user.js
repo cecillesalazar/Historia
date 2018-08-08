aba2976010704d2bef1234025e68b339282260f9
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-08 08:55:52 -0700

    Moved endpoints to routes/user.js and exported the router

--- back_end/routes/user.js
+++ back_end/routes/user.js
@@ -3,3 +3,59 @@ const router = express.Router();
 
 const User = require('../user-schema.js');
 const mongoose = require('mongoose');
+
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  User
+    .findById(id)
+    .then(result => {
+      res.json(result)
+    })
+    .catch(err => {
+      next(err)
+    })
+})
+
+router.post('/', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const newUser = { username, password }
+  User
+    .create(newUser)
+    .then(result => {
+      res
+        .location(`${req.originalUrl}`)
+        .status(201)
+        .json(result)
+    })
+    .catch(err => {
+      next(err)
+    })
+})
+
+router.put('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const { saveFile } = req.body;
+  const updatedUser = { saveFile };
+
+  if(saveFile) {
+    User.findById(id, function(err, user) {
+      if(err) {
+        next(err);
+      } else {
+        user.saveFile = saveFile;
+        user.save((err, saveUser) => {
+          if(err) {
+            next(err);
+          } else {
+            res.location(`${req.originalUrl}`).status(201).json(saveUser)
+          }
+        })
+      }
+    })
+  }
+})
+
+module.exports = router;
