8a52aa43c3a37b28652b26e7630b22be02168be6
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-08 15:07:29 -0700

    Implemented missing Router tags in index.js

--- front_end/src/index.js
+++ front_end/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
-import Menu from './components/menu';
+import App from './components/app.js';
 import store from './store';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Menu />
+    <Router>
+      <App />
+    </Router>
   </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
