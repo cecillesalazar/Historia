cfe3c49e8682eab83df79bb4e4e4708c604cf142
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-09 09:04:10 -0700

    Edited backendApi.js to have a story array instead of string and a speakingOrder array

--- front_end/src/backendApi.js
+++ front_end/src/backendApi.js
@@ -53,7 +53,8 @@ const STORY_GRAPH_MONKEY_PAW = {
     options: [
       COVETS_PAW
     ],
-    story: 'Plot for SERG_TELLS_STORY',
+    story: ['It was a dark and stormy night. The White household hunkered down by the hearth, father and son at chess, with mother knitting placidly by the fire', 'Such strong winds tonight!', 'Said Mr. White, who, making an embarassing blunder, much too late to change, hoped to distract young Herbert', 'Certainly so.', 'Said Herbert, who, despite his father\'s aside, had all his faculties about him, and quickly spotted the opening', 'Check.'],
+    speakingOrder: ['Narrator', 'Mr. White', 'Narrator', 'Herbert', 'Narrator', 'Herbert'],
     button: [
       'Next: Mr. White Covets the Paw'
     ],
