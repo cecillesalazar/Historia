2434f1b591892810a1d53337266b9714492ecf08
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-09 11:00:46 -0700

    Added actions for changing the story and speaker indexes

--- front_end/src/actions/actions.js
+++ front_end/src/actions/actions.js
@@ -2,7 +2,19 @@ const CHANGE_NODE = 'CHANGE_NODE';
 export const changeNode = (currentNode) => ({
   type: CHANGE_NODE,
   currentNode,
-})
+});
+
+const CHANGE_STORY_INDEX = 'CHANGE_STORY_INDEX';
+export const changeStoryIndex = (num) => ({
+  type: CHANGE_STORY_INDEX,
+  num
+});
+
+const CHANGE_SPEAKER_INDEX = 'CHANGE_SPEAKER_INDEX';
+export const changeSpeakerIndex = (num) => ({
+  type: CHANGE_SPEAKER_INDEX,
+  num
+});
 
 const SAVE_FILE_SUCCESS = 'SAVE_FILE_SUCCESS';
 export const saveFileSuccess = storyNode => ({
@@ -14,16 +26,16 @@ const CHANGE_STORY = 'CHANGE_STORY';
 export const changeStory = (story) =>({
   type: CHANGE_STORY,
   story
-})
+});
 
 const DISPLAY_GAMEPLAY = 'DISPLAY_GAMEPLAY';
 export const displayGameplay = (bool) => ({
   type: DISPLAY_GAMEPLAY,
   bool
-})
+});
 
 const DISPLAY_TUTORIAL = 'DISPLAY_TUTORIAL';
 export const displayTutorial = (bool) => ({
   type: DISPLAY_TUTORIAL,
   bool
-})
+});
