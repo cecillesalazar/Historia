52994f2b042661bc6e87b89935066eebae318e08
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-13 21:43:13 -0700

    Styled logout button

--- front_end/src/components/header-bar.js
+++ front_end/src/components/header-bar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {clearAuth} from '../actions/auth';
 import {clearAuthToken} from '../local-storage';
+import './components-css/header-bar.css';
 
 export class HeaderBar extends React.Component {
     logOut() {
@@ -14,7 +15,7 @@ export class HeaderBar extends React.Component {
         let logOutButton;
         if (this.props.loggedIn) {
             logOutButton = (
-                <button onClick={() => this.logOut()}>Log out</button>
+                <button className="logout-button" onClick={() => this.logOut()}>Log out</button>
             );
         }
         return (
