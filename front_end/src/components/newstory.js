b79ab3f7a7cec5604f48c1dd323e91178d1c2840
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-14 09:20:54 -0700

    Styled Dashboard Button

--- front_end/src/components/newstory.js
+++ front_end/src/components/newstory.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { getRandomStory } from '../backendApi.js';
 import { changeStory } from '../actions/actions.js';
+import './components-css/newstory.css';
 
 export default class NewStory extends React.Component {
   render() {
     if(this.props.options.length === 0) {
       return (
        <div>
-         <button type="button" onClick={() => getRandomStory().then((story) => this.props.dispatch(changeStory(story)))}>Play New Story</button>
+         <button className="newstory-button" type="button" onClick={() => getRandomStory().then((story) => this.props.dispatch(changeStory(story)))}>Play New Story</button>
        </div>
       )
     } else {
--- front_end/src/components/returndashboard.js
+++ front_end/src/components/returndashboard.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import './components-css/returndashboard.css'
 
 class ReturnDashboard extends React.Component {
   render() {
     return (
           <div>
-            <button type="button" onClick={this.props.displayDashboard}>Back to Dashboard</button>
+            <button className="dashboard-button" type="button" onClick={this.props.displayDashboard}>Back to Dashboard</button>
           </div>
     );
   }
