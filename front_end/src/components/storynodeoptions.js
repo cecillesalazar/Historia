c6fecfae309d745fb7ac4d6bd836ff60150c00d6
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-14 13:46:01 -0700

    Styled Story Change Button

--- front_end/src/components/storynodeoptions.js
+++ front_end/src/components/storynodeoptions.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class StoryNodeOptions extends React.Component {
   render() {
     const options = this.props.options.map((option, index) => (
-      <button key={option} type="button" onClick={() => {
+      <button className="story-change-button" key={option} type="button" onClick={() => {
         this.props.resetStoryIndex();
         this.props.changeStoryNode(option)}}>
         {this.props.buttons[index]}
