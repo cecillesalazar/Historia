c25d623219a339141849b8e7019a2306d3f2bee2
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-14 17:33:25 -0700

    Installed 2 react typing libraries and animated some text as an example

--- front_end/src/components/storynodescript.js
+++ front_end/src/components/storynodescript.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './components-css/storynodescript.css';
+import Typing from 'react-typing-animation';
 
 export default class StoryNodeScript extends React.Component {
   render() {
@@ -9,8 +10,10 @@ export default class StoryNodeScript extends React.Component {
           <p>{this.props.speaker}</p>
         </div>
         <div className="dialogue-box">
-          <p className="dialogue">{this.props.story}</p>
-          {this.props.children}
+          <Typing>
+            <p className="dialogue">{this.props.story}</p>
+          </Typing>
+            {this.props.children}
         </div>
       </div>
     )
