35ba3b4dda81b4f306c71682b306a803a589e19f
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-16 22:15:33 -0700

    Finalized story schema

--- back_end/story-schema.js
+++ back_end/story-schema.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 
 const storySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  description: { type: String, required: true },
-  startNode: { type: String, required: true },
-  storyGraph: { type: Object, required: true },
-  speakers: { type: Object, required: true },
-})
+});
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model('Story', storySchema, 'stories');
