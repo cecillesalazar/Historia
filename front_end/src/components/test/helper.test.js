c212b6661c02e9b57d475f459a8f4d1a30759289
Cecille Salazar <cecillersalazar@gmail.com>
2018-08-17 07:24:33 -0700

    Deleted test for helper.js - obsolete

--- front_end/src/components/test/helper.test.js
+++
@@ -1,7 +0,0 @@
-import helper from '../test/helper.js';
-
-describe('helper', () => {
-  it('should return the story node for the key passed', () => {
-      expect(helper.getStoryNode(helper.BAD_ENDING)).toMatchSnapshot();
-  });
-});
